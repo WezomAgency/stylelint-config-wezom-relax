60fdc5d338951230098ce2d0ac6afbdabf7d4815
Олег Дутченко <dutchenko.o.dev@gmail.com>
2020-01-16 01:13:31 +0200

    0.0.1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -26,7 +26,10 @@ tests.forEach((test) => {
 
 		it('flags no warnings', () => {
 			return result.then((data) =>
-				expect(data.results[0].warnings).toHaveLength(0)
+				const warnings = data.results[0].warnings.filter(
+					(warning) => warning.rule === test
+				);
+				expect(warnings).toHaveLength(0);
 			);
 		});
 	});
